de138c43cd50fac110e0af1485756643fe4d3a0d
“Vilde <vilde@vevatne.no>
2017-07-31 13:10:35 -0700

    Create books reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+	state: (state = {}) => state
 });
 
 export default rootReducer;
+
+// reducer = a function that returns a piece of the applications state
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+	return [
+		{ title: 'Javascript: The Good Parts' },
+		{ title: 'Harry Potter' },
+		{ title: 'The Dark Tower' },
+		{ title: 'Eloquent Ruby' }
+	];
+}
+
+// Step 1: Create reducer
+// Step 2: Wire it into the application
