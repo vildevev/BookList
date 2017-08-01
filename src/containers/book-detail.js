6e3d1ff3a59be9f61ce455f684b2d000a2827be9
“Vilde <vilde@vevatne.no>
2017-07-31 20:40:29 -0700

    Add mapStateToProp function

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
 	render() {
-		return <div>Book Detail!</div>;
+		return (
+			<div>
+				<h3>Details for:</h3>
+				<div>{this.props.book.title}</div>
+			</div>;
+		)
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		book: state.activeBook
+	};
+}
+
+export default connect(mapStateToProps)(BookDetail);
