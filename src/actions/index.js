a6a3ff37b7921b12eaa399135ad8b978bb4b67f4
“Vilde <vilde@vevatne.no>
2017-07-31 20:24:05 -0700

    Add functionality for ActionCreator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
 // action creater has to be wired up to redux. Binding the action the the component
 export function selectBook(book) {
-	console.log('A book has been selected:', book.title);
+	// selectBook is an ActionCreator, it needs to return an action, an object with a type property
+	return {
+		// an uppercase
+		type: 'BOOK_SELECTED',
+		// payload is more information about the action
+		payload: book
+	};
 }
