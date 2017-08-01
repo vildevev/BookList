80d9e2c4cca581c9d0230834cb1bd47e83d7dc15
“Vilde <vilde@vevatne.no>
2017-08-01 10:27:11 -0700

    Add handler for when props are null

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,12 +3,20 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
 	render() {
+		if (!this.props.book) {
+			return <div>Select a book to get started.</div>;
+		}
 		return (
 			<div>
 				<h3>Details for:</h3>
-				<div>{this.props.book.title}</div>
-			</div>;
-		)
+				<div>
+					Title: {this.props.book.title}
+				</div>
+				<div>
+					Pages: {this.props.book.pages}
+				</div>
+			</div>
+		);
 	}
 }
 
